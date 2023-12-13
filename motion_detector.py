eeee21955646e2f16c0c13285ebdb5dcc08d5e54
Joao Marques Desktop Windows <johnmarques.br2002@gmail.com>
2023-12-13 01:38:44 -0500

    sending whatsapp ok and photo ok, but needs to work on video generation

--- motion_detector.py
+++ motion_detector.py
@@ -1,12 +1,22 @@
 import cv2, time, pandas
 from datetime import datetime
+import requests
+import sys
 
 first_frame = None
 status_list = [None,None]
 times = []
 df=pandas.DataFrame(columns=["Start","End"])
 
-video = cv2.VideoCapture(0)
+api_url = "http://10.0.0.54:3333/message/image?key=memorukey"
+phone_number = "15196154641"    
+
+video = cv2.VideoCapture(1)
+
+#video writer object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
 
 while True:
     check, frame = video.read()
@@ -31,9 +41,40 @@ while True:
 
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+     
+        data = {
+            'id' : phone_number,
+            'message' : "There's been moviment in your room",
+        }
+        try:    
+            print("printing image")
+            whatsapp_image = 'image.jpg'
+            # whatsapp_image = "D:\$$FanshaweServer\ENGLISH\WhatsApp Image 2023-12-07 at 11.02.41 AM(1).jpeg"
+            cv2.imwrite(whatsapp_image, frame)
+            whatsapp_file = {'file': open(whatsapp_image, 'rb')}
+            out.write(frame)
+            curl_command = [
+                'curl',
+                '-X', 'POST',
+                '-F', f"id={phone_number}",
+                '-F', f"message=There's been movement in your room",
+                '-F', f"file=@{whatsapp_image}",
+                api_url
+                ]
+            # result = subprocess.run(curl_command, capture_output=True, text=True, check=True)
+
+        except requests.exceptions.RequestException as e:
+            print("Request failed:", e)
+    
+
+        
     status_list.append(status)
 
     status_list=status_list[-2:]
+    
+    if status == 0 :
+        out.release()
+        # cv2.destroyAllWindows()        
 
 
     if status_list[-1]==1 and status_list[-2]==0:
@@ -42,9 +83,9 @@ while True:
         times.append(datetime.now())
 
 
-    cv2.imshow("Gray Frame",gray)
-    cv2.imshow("Delta Frame",delta_frame)
-    cv2.imshow("Threshold Frame",thresh_frame)
+    # cv2.imshow("Gray Frame",gray)
+    # cv2.imshow("Delta Frame",delta_frame)
+    # cv2.imshow("Threshold Frame",thresh_frame)
     cv2.imshow("Color Frame",frame)
 
     key=cv2.waitKey(1)
@@ -52,15 +93,15 @@ while True:
     if key == ord('q'):
         if status == 1:
             times.append(datetime.now())
+        
+        if video.isOpened :
+            video.release()
+            cv2.destroyAllWindows
+            sys.exit()
         break
 
 print(status_list)
 print(times)
-
-for i in range(0, len(times), 2):
-    df = df.append({"Start": times[i],"End": times[i+1]}, ignore_index=True)
-
-df.to_csv("Times.csv")
 
 video.release()
 cv2.destroyAllWindows
